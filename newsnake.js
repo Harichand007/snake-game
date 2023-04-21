d78c2665b7fb408c56a5d6ccf47f91c72e6604ea
Harichand007 <131490996+Harichand007@users.noreply.github.com>
2023-04-22 04:34:05 +0530

    Second-commit

--- newsnake.js
+++ newsnake.js
@@ -9,18 +9,24 @@ class SnakePart{
     }
 }
 
-let speed = 7;
-let tilecount=20;
-let tilesize=canvas.width/tilecount-2;
-let headx = 10;
-let heady = 10; 
-let xvelocity=0;
-let yvelocity =0;
-let applex=5;
-let appley=5;
+var speed = 5;
+var tilecount=20;
+var tilesize=canvas.width/tilecount-2;
+var headx = 10;
+var heady = 10; 
+var xvelocity=0;
+var yvelocity =0;
+var applex=5;
+var appley=5;
 const snakeParts=[];
-let taillength=1;
-let score=0;
+var taillength=0;
+var score=0;
+var value=Math.floor(Math.random()*10);
+
+    
+    
+
+
 
 
  
@@ -32,7 +38,7 @@ function drawgame(){
     }
     clearscreen();
    
-
+text();
     drawsnake(); 
     drawapple(); 
     checkapplecollision();
@@ -40,9 +46,29 @@ function drawgame(){
     setTimeout(drawgame, 1000/ speed);
 }
 function isgameover(){
+
     let gameover=false;
+    if (yvelocity===0&&xvelocity===0)
+    return false;
     if(headx<0||heady<0){
     gameover=true;
+}
+else if (headx>=tilecount||heady>=tilecount){
+    gameover=true;
+
+} 
+for(let i=0;i<snakeParts.length;i++){
+    let part =snakeParts[i];
+    if(part.x===headx&& part.y===heady){
+        gameover=true;
+        break;
+    }
+}
+if (gameover){
+    ctx.fillStyle='white';
+    ctx.font ='50px Verdana';
+   
+    ctx.fillText("Game Over!",canvas.width/6.5,canvas.height/2)
 }
 return gameover;
     }
@@ -68,20 +94,31 @@ function chnagesnakeposition(){
     headx=headx+xvelocity;
     heady=heady+yvelocity;
 }
-function drawapple(){
+function drawapple(){ 
     ctx.fillStyle ='red';
-        ctx.fillRect(applex*tilecount,appley*tilecount,tilesize,tilesize) 
-
+        ctx.fillRect(applex*tilecount,appley*tilecount,tilesize,tilesize)
 }
+
+ function text(){
+     ctx.fillStyle="white";
+     ctx.font ="10px Verdana" 
+    ctx.fillText("Score" +score,canvas.width-50,10);
+
+ }
 function checkapplecollision(){
     if(applex===headx&&appley==heady){
         applex=Math.floor(Math.random()*tilecount);
         appley=Math.floor(Math.random()*tilecount);
-        taillength++;
-        score++;
+        var value=Math.floor(Math.random()*3);
+            ctx.fillStyle="white";
+            ctx.font ="50px Verdana" 
+            ctx.fillText(Math.floor(Math.random()*value),canvas.width-200,50);
+            taillength=taillength+value;
+            score=score+value;    
+        }       
     }
 
-}
+
 function drawscore(){
     ctx.fillStyle="white";
     ctx.font ="10px Verdana" 
